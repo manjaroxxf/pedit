20c3cbb3328f0eb7d5d66727dd23cccd728178c8
ChungZH <chungzh07@gmail.com>
2020-06-24 18:32:32 +0800

    feat: line numbers & folding

--- src/ptextedit.h
+++ src/ptextedit.h
@@ -3,12 +3,15 @@
 
 #include <repository.h>
 
+#include <QMenu>
 #include <QPlainTextEdit>
 
 namespace KSyntaxHighlighting
 {
 class SyntaxHighlighter;
 }
+
+class PTextEditSidebar;
 
 class PTextEdit : public QPlainTextEdit
 {
@@ -22,11 +25,43 @@ class PTextEdit : public QPlainTextEdit
   void saveFile(const QString &fileName);
   void setCurrentFile(const QString &fileName);
 
+ protected:
+  void contextMenuEvent(QContextMenuEvent *event) override;
+  void resizeEvent(QResizeEvent *event) override;
+
  private:
+  friend class PTextEditSidebar;
+
+  int sidebarWidth() const;
+  void sidebarPaintEvent(QPaintEvent *event);
+  void updateSidebarGeometry();
+  void updateSidebarArea(const QRect &rect, int dy);
+
+  QTextBlock blockAtPosition(int y) const;
+  bool isFoldable(const QTextBlock &block) const;
+  bool isFolded(const QTextBlock &block) const;
+  void toggleFold(const QTextBlock &block);
+
   void highlightCurrentLine();
   void setTheme(const KSyntaxHighlighting::Theme &theme);
   KSyntaxHighlighting::Repository m_repository;
   KSyntaxHighlighting::SyntaxHighlighter *m_highlighter;
+  PTextEditSidebar *sidebar;
+};
+
+class PTextEditSidebar : public QWidget
+{
+  Q_OBJECT
+ public:
+  explicit PTextEditSidebar(PTextEdit *pTextEdit);
+  QSize sizeHint() const override;
+
+ protected:
+  void paintEvent(QPaintEvent *event) override;
+  void mouseReleaseEvent(QMouseEvent *event) override;
+
+ private:
+  PTextEdit *m_pTextEdit;
 };
 
 #endif  // PTEXTEDIT_H
