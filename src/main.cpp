7ea9d3190cc7af55c0bf11d7acea6b984b9717f2
ChungZH <chungzh07@gmail.com>
2020-07-02 12:25:07 +0800

    version: 0.1.0

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    QApplication::setApplicationVersion("0.1.0");
+
     MainWindow *w = new MainWindow();
     w->show();
     return a.exec();
